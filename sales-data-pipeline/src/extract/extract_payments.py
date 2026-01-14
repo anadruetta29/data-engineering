e12786fdadd315eb44d43b5df74db659f4f2e32f
anadruetta29 <anadruetta2023@gmail.com>
2026-01-14 01:49:33 -0300

    add: extract scripts
    only reads data raw and saves it

--- sales-data-pipeline/src/extract/extract_payments.py
+++ sales-data-pipeline/src/extract/extract_payments.py
@@ -1,6 +1,16 @@
-import requests
+import json
 import pandas as pd
+from pathlib import Path
+
+RAW_PATH = Path("data/raw")
+PAYMENTS_FILE = RAW_PATH / "payments_raw.json"
 
 def extract_payments():
-    response = requests.get("API_URL")
-    return pd.DataFrame(response.json())
+
+    if not PAYMENTS_FILE.exists():
+        raise FileNotFoundError("payments_raw.json not found")
+
+    with open(PAYMENTS_FILE, "r") as f:
+        data = json.load(f)
+
+    return pd.DataFrame(data)
--- sales-data-pipeline/src/extract/extract_sales.py
+++ sales-data-pipeline/src/extract/extract_sales.py
@@ -1,4 +1,15 @@
 import pandas as pd
+from pathlib import Path
+
+RAW_PATH = Path("data/raw")
+PAYMENTS_FILE = RAW_PATH / "sales.csv"
 
 def extract_sales():
-    return pd.read_csv("data/raw/sales.csv")
+    if not PAYMENTS_FILE.exists():
+        raise FileNotFoundError("sales.csv not found")
+
+    RAW_PATH.mkdir(parents=True, exist_ok=True)
+
+    df = pd.read_csv(RAW_PATH / "sales.csv")
+
+    return df
